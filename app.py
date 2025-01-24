ad6c0015bd7a0c82a7d187b1e531c604c7b99c53
Waree01 <nongningwarisara@gmail.com>
2025-01-24 14:08:31 +0700

    สร้างคงาสแรก Github repo

---
+++ app.py
@@ -0,0 +1,11 @@
+class Student(object):
+  studen_count = 0
+
+def __new__(self):
+  print("Student._nem_')
+
+def __init__(self):
+  print("Stdent._init_')
+
+def dtudy_hard(self):
+  print('sir, yessir,')
